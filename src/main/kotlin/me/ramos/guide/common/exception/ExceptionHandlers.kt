42340729eb08d084f172ae62f173358da14ccd95
alanhakhyeonsong <songs4805@naver.com>
2024-03-05 13:29:27 +0900

    fix: API 예외 응답 포맷 fix

--- src/main/kotlin/me/ramos/guide/common/exception/ExceptionHandlers.kt
+++ src/main/kotlin/me/ramos/guide/common/exception/ExceptionHandlers.kt
@@ -4,6 +4,8 @@ import me.ramos.guide.common.enum.CodeEnum
 import me.ramos.guide.common.response.CommonResponse
 import me.ramos.guide.common.util.Logger.Companion.log
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -12,17 +14,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class ExceptionHandlers {
 
     @ExceptionHandler(BaseException::class)
-    fun handleCustomException(e: BaseException): CommonResponse<Nothing> {
+    fun handleCustomException(e: BaseException): ResponseEntity<CommonResponse<Nothing>> {
         // TODO: log format 정의할 것
         log.error { "message: ${e.returnMessage}, code: ${e.returnCode}"}
-        return CommonResponse(e.returnCode, e.returnMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(CommonResponse(e.returnCode, e.returnMessage))
     }
 
     @ExceptionHandler(Exception::class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun handleException(e: Exception): CommonResponse<Nothing> {
+    fun handleException(e: Exception): ResponseEntity<CommonResponse<Nothing>> {
         // TODO: log format 정의할 것
         log.error { "message: ${e.message}, code: ${HttpStatus.INTERNAL_SERVER_ERROR.value()}"}
-        return CommonResponse(CodeEnum.INTERNAL_SERVER_ERROR.code, CodeEnum.INTERNAL_SERVER_ERROR.message);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(CommonResponse(CodeEnum.INTERNAL_SERVER_ERROR.code, CodeEnum.INTERNAL_SERVER_ERROR.message))
     }
 }
