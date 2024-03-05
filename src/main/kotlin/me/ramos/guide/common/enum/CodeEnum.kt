a9169256c42ef0fd319ced5ee39ad99a3fa43b8f
alanhakhyeonsong <songs4805@naver.com>
2024-03-05 14:21:36 +0900

    feat: API 정상 응답 enum 추가

--- src/main/kotlin/me/ramos/guide/common/enum/CodeEnum.kt
+++ src/main/kotlin/me/ramos/guide/common/enum/CodeEnum.kt
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus
 enum class CodeEnum(val code: Int, val message: String) {
     // 에러
     BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다"),
+    PLAYER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "선수를 찾을 수 없습니다"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 에러"),
-    // TODO: 정상 응답 코드 지정할 것
+    // 정상 응답
+    OK(HttpStatus.OK.value(), "success")
 }
